9ed02fbc68c1fb1d284870170b1ede1660f2c144
praivesi <praivesi@gmail.com>
2021-10-27 23:58:32 +0900

    add MakeBigNumber in Greedy Package (프로그래머스: 큰 수 만들기)

--- AlgorithmsCPP/MakeBigNumber.cpp
+++ AlgorithmsCPP/MakeBigNumber.cpp
@@ -1,7 +1,47 @@
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
 string solution(string number, int k) {
+	stack<char> stk;
+
+	for (int i = 0; i < number.size(); i++) {
+		if (k != 0) {
+			while (!stk.empty()) {
+				if (stk.top() >= number[i]) { break; }
+				if (k == 0) { break; }
+
+				stk.pop();
+				k--;
+			}
+		}
+
+		stk.push(number[i]);
+	}
+
+	while (k != 0) {
+		stk.pop();
+		k--;
+	}
+
+	int charsSize = stk.size() + 1;
+	char* chars = new char[charsSize];
+
+	int charIdx = stk.size() - 1;
+	while (!stk.empty()) {
+		chars[charIdx] = stk.top();
+		stk.pop();
+
+		charIdx--;
+	}
+
+	chars[charsSize - 1] = '\0';
+
+	return string(chars);
+}
+
+int main() {
+	string ret = solution("4177252841", 4);
 }
