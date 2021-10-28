e4fdb8a9556c6c197482b2c8035ce2069f521419
praivesi <praivesi@gmail.com>
2021-10-28 22:55:47 +0900

    add 124World in Level2 Package (프로그래머스: 124나라)

--- AlgorithmsCPP/124World.cpp
+++ AlgorithmsCPP/124World.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -7,26 +8,57 @@ using namespace std;
 string solution(int n);
 
 int main() {
-	string ret = solution(10);
+	string ret = solution(15);
 
 	cout << ret << endl;
 }
 
 string solution(int n) {
+	if (n == 1) { return "1"; }
+	if (n == 2) { return "2"; }
+	if (n == 3) { return "4"; }
 
-	string ret = "";
+	int quotient = 0;
+	int remainder = 0;
 
+	vector<int> remainders;
 
-	int k = 1;
-	int cnt = 0;
-	while (k < n) {
-		k *= 3;
-		cnt++;
+	while (n > 3) {
+		quotient = n / 3;
+		remainder = n % 3;
+
+		if (remainder == 0) {
+			quotient--;
+			remainder += 3;
+		}
+
+		remainders.push_back(remainder);
+
+		n = quotient;
 	}
 
-	k /= 3;
-	n -= k;
+	remainders.push_back(quotient);
 
+	reverse(remainders.begin(), remainders.end());
 
-	return ret;
+	char* chars = new char[remainders.size() + 1];
+
+	for (int i = 0; i < remainders.size(); i++)
+	{
+		switch (remainders[i]) {
+		case 1:
+			chars[i] = '1';
+			break;
+		case 2:
+			chars[i] = '2';
+			break;
+		case 3:
+			chars[i] = '4';
+			break;
+		}
+	}
+
+	chars[remainders.size()] = '\0';
+
+	return string(chars);
 }
